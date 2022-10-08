5e49b5c68a87544444970cb269fcb9f2d23df56c
Gavin Bogie <39637171+LegendaryBob13@users.noreply.github.com>
2022-10-08 04:20:35 +0000

    yolo

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import EnvironmentPlugin from 'vite-plugin-environment';
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), EnvironmentPlugin(['API_KEY', 'AUTH_DOMAIN', 'PROJECT_ID', 'STORAGE_BUCKET', 'MESSAGING_SENDER_ID', 'APP_ID', 'MEASUREMENT_ID'])],
+    ssr: { target: "webworker", noExternal: true },
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      EnvironmentPlugin([
+        "API_KEY",
+        "AUTH_DOMAIN",
+        "PROJECT_ID",
+        "STORAGE_BUCKET",
+        "MESSAGING_SENDER_ID",
+        "APP_ID",
+        "MEASUREMENT_ID",
+      ]),
+    ],
   };
 });
